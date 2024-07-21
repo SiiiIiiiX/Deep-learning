e7c2abc1a8c11a6ea84715a2f8db2ece8186bfc0
SiiiIiiiX <2291753075@qq.com>
2024-07-21 15:17:58 +0800

    Add files via upload

--- pytorch_rnn.py
+++ pytorch_rnn.py
@@ -1,19 +1,30 @@
 import torch
 import torch.nn as nn
+from torchviz import make_dot
 
-input_size = 100   # 输入数据编码的维度
-hidden_size = 20   # 隐含层维度
-num_layers = 4     # 隐含层层数
+class SimpleRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(SimpleRNN, self).__init__()
+        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers)
 
-rnn = nn.RNN(input_size=input_size,hidden_size=hidden_size,num_layers=num_layers)
-print("rnn:",rnn)
+    def forward(self, x, h0):
+        out, h = self.rnn(x, h0)
+        return out, h
 
-seq_len = 10        # 句子长度
+# 超参数设置
+input_size = 100  # 输入数据编码的维度
+hidden_size = 20  # 隐含层维度
+num_layers = 4  # 隐含层层数
+seq_len = 10  # 序列长度
 batch_size = 1
-x = torch.randn(seq_len,batch_size,input_size)        # 输入数据
-h0 = torch.zeros(num_layers,batch_size,hidden_size)   # 输入数据
 
-out, h = rnn(x, h0)  # 输出数据
-
+model = SimpleRNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers)
+x = torch.randn(seq_len, batch_size, input_size)  # 输入数据
+h0 = torch.zeros(num_layers, batch_size, hidden_size)  # 初始隐藏状态
+out, h = model(x, h0)
 print("out.shape:",out.shape)
 print("h.shape:",h.shape)
+dot = make_dot(out, params=dict(model.named_parameters()))
+dot.format = 'pdf'
+dot.save(filename='rnn_model1')
+
